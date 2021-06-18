7bf948b83bcf64a5b354113cc22c66ea9f383fc7
rabbit-22 <chlwlal20@gmail.com>
2021-06-18 09:26:34 +0900

    모바일 헤더 위치 변경 및 슬라이드 변경

--- frontPageTemplate/scripts/script.js
+++ frontPageTemplate/scripts/script.js
@@ -1,38 +1,36 @@
-const slideList = document.querySelector('.main__slide-list');
-const slideContents = document.querySelectorAll('.main__study-slide');
-const slideBtnNext = document.querySelector('.main__study-buttonB');
-const slideBtnPrev = document.querySelector('.main__study-buttonA'); 
-const slideLen = slideContents.length; 
-const slideWidth = 130;
-const slideSpeed = 300; 
-const startNum = 300;
-const tablet = window.matchMedia("all and (min-width:768px) and (max-width:1023px)");
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    observer: true,
+    observeParents: true,
+    spaceBetween: 24,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        1280: {
+            slidesPerView: 5.5,
+            slidesPerGroup:5.5,
+        },
+        600: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+        }
 
-slideList.style.width = slideWidth * (slideLen) + "px";
-let curIndex = 0; 
-
-
-
-slideBtnNext.addEventListener('click', function() {
-    if (curIndex < slideLen - 4) { //스터디 개수 증가하면 숫자를 더 낮은 수로 변경
-        slideList.style.transition = slideSpeed + "ms";
-        slideList.style.transform = "translate3d(-" + (slideWidth * (curIndex + 1)) + "px, 0px, 0px)";
-        curSlide = slideContents[++curIndex];
-    } 
-    
-    
-});
-
-
-slideBtnPrev.addEventListener('click', function() {
-    curSlide = slideContents[--curIndex];
-    if (curIndex >= 0) {
-        slideList.style.transition = slideSpeed + "ms";
-        slideList.style.transform = "translate3d(-" + (slideWidth * curIndex) + "px, 0px, 0px)";
-        
     }
-});
+  });
 
+
+function openNav() {
+    document.getElementById('mysidenav').style.width = '250px';
+}
+function closeNav() {
+    document.getElementById('mysidenav').style.width = '0';
+}
 
 
 
