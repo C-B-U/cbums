29c88921c09a38282ac7a484f4ccc89511a9e61c
rabbit-22 <chlwlal20@gmail.com>
2021-06-27 01:53:36 +0900

    default페이지 js 분리 및 스터디 개설 페이지 추가

--- frontPageTemplate/scripts/study_make.js
+++ frontPageTemplate/scripts/study_make.js
@@ -2,31 +2,24 @@ const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
     loop: false,
-    slidesPerView: 3.1,
-    slidesPerGroup: 3.1,
-    observer: true,
-    observeParents: true,
-    spaceBetween: 24,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    debugger: false,
+    mousewheel: false,
+    touchRatio: 0,
+    pagination:{
+        el:'.swiper-pagination',
+    },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
     breakpoints: {
-        1280: {
-            slidesPerView: 5.5,
-            slidesPerGroup:5.5,
-        },
+        
         600: {
-            slidesPerView: 4,
-            slidesPerGroup: 4,
+           
         }
 
     }
   });
-
-
-
-
-
-
 
