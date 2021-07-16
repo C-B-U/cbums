d618fb8de985b47da890724cf0d81733842cd78f
rabbit-22 <chlwlal20@gmail.com>
2021-07-16 11:48:32 +0900

    글쓰기 작성 페이지 (js 수정 필요)

--- frontPageTemplate/scripts/study/study_workJournalWriting/study_workJournalWriting.js
+++ frontPageTemplate/scripts/study/study_workJournalWriting/study_workJournalWriting.js
@@ -1,12 +1,12 @@
-function setThumbnail(event) { 
-    for (var image of event.target.files) { 
-        var reader = new FileReader(); 
-        reader.onload = function(event) { 
-            var img = document.createElement("img"); 
-            img.setAttribute("src", event.target.result); 
-            document.querySelector("image_container").appendChild(img); 
-        }; 
-            console.log(image); 
-            reader.readAsDataURL(image); 
-        } 
-    }
+var button = document.getElementsByClassName('main__writing-table-button'),
+    arr = ["결석","지각","출석"],
+    color = ["#fff6b1","#b8ff8f", "#ffb9b9"];
+
+for (var i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', function(){
+        if(button[i].innerText === arr[i]){
+            button[i].innerText = arr[(i + 1) % 3];
+            button[i].style.backgroundColor=color[(i + 1) % 3];
+        }
+    });
+}
