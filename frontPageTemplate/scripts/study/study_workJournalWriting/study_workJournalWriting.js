f6393bf1eaf63e498ea1805295e3d322e0d99bbb
rabbit-22 <chlwlal20@gmail.com>
2021-07-17 13:27:43 +0900

    input 변경

--- frontPageTemplate/scripts/study/study_workJournalWriting/study_workJournalWriting.js
+++ frontPageTemplate/scripts/study/study_workJournalWriting/study_workJournalWriting.js
@@ -1,12 +1,22 @@
 var button = document.getElementsByClassName('main__writing-table-button'),
     arr = ["결석","지각","출석"],
-    color = ["#fff6b1","#b8ff8f", "#ffb9b9"];
+    color = ["#ffb9b9", "#fff6b1", "#b8ff8f"]
 
 for (var i = 0; i < button.length; i++) {
-    button[i].addEventListener('click', function(){
-        if(button[i].innerText === arr[i]){
-            button[i].innerText = arr[(i + 1) % 3];
-            button[i].style.backgroundColor=color[(i + 1) % 3];
-        }
-    });
+    button[i].addEventListener('click', change(i));
+}
+
+function change(i){
+    if(button[i].value === arr[0]){
+        button[i].style.backgroundColor = color[1];
+        button[i].value = arr[1];
+    }
+    else if(button[i].value === arr[1]){
+        button[i].style.backgroundColor = color[2];
+        button[i].value = arr[2];
+    }
+    else if(button[i].value === arr[2]){
+        button[i].style.backgroundColor = color[0];
+        button[i].value = arr[0];
+    }
 }
