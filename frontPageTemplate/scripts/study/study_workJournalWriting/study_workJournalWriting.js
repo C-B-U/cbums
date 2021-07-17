dab13eeed02a2eecf36917736b234465276ea6d8
rabbit-22 <chlwlal20@gmail.com>
2021-07-17 15:15:50 +0900

    활동일지 글쓰기 페이지

--- frontPageTemplate/scripts/study/study_workJournalWriting/study_workJournalWriting.js
+++ frontPageTemplate/scripts/study/study_workJournalWriting/study_workJournalWriting.js
@@ -1,22 +1,19 @@
-var button = document.getElementsByClassName('main__writing-table-button'),
-    arr = ["결석","지각","출석"],
-    color = ["#ffb9b9", "#fff6b1", "#b8ff8f"]
+let attend = document.getElementsByClassName('main__writing-table-attend'),
+    color = ["#ffb9b9", "#fff6b1", "#b8ff8f"];
 
-for (var i = 0; i < button.length; i++) {
-    button[i].addEventListener('click', change(i));
-}
+function switchValue(element, stu) {
+    if(element.innerText=='결석') {
+        element.innerText = '지각';
+        element.style.backgroundColor = color[1];
+        attend.item(stu).value = '1';
 
-function change(i){
-    if(button[i].value === arr[0]){
-        button[i].style.backgroundColor = color[1];
-        button[i].value = arr[1];
-    }
-    else if(button[i].value === arr[1]){
-        button[i].style.backgroundColor = color[2];
-        button[i].value = arr[2];
-    }
-    else if(button[i].value === arr[2]){
-        button[i].style.backgroundColor = color[0];
-        button[i].value = arr[0];
+    }else if(element.innerText=='지각') {
+        element.innerText = '출석'
+        element.style.backgroundColor = color[2];
+        attend.item(stu).value = '2';
+    }else {
+        element.innerText = '결석'
+        element.style.backgroundColor = color[0];
+        attend.item(stu).value = '0';
     }
 }
