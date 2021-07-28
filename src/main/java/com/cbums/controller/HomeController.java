6415e5bf9fff04185bf3d005e23c072a04cbe0a5
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-07-28 17:09:39 +0900

    시큐리티 설정 전 뷰 간단 제작

--- src/main/java/com/cbums/controller/HomeController.java
+++ src/main/java/com/cbums/controller/HomeController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String defaultPage() {
-        return "default";
+        return "default/default";
     }
 }
