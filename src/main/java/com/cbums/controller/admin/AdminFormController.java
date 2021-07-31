baa15afc40fe7f36ef3f962eee5b279c124828f0
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-07-31 19:26:53 +0900

    AdminController 디렉토리 수정

---
+++ src/main/java/com/cbums/controller/admin/AdminFormController.java
@@ -0,0 +1,25 @@
+package com.cbums.controller.admin;
+
+import com.cbums.model.Form;
+import com.cbums.model.Member;
+import com.cbums.service.FormService;
+import com.cbums.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/**")
+@RequiredArgsConstructor
+public class AdminFormController {
+
+    private final FormService formService;
+
+    @GetMapping("/form")
+    public List<Form> formViewPage() {
+        return formService.getForms();
+    }
+}
--- src/main/java/com/cbums/controller/admin/AdminMemberController.java
+++ src/main/java/com/cbums/controller/admin/AdminMemberController.java
@@ -1,8 +1,7 @@
-package com.cbums.controller;
+package com.cbums.controller.admin;
 
 import com.cbums.model.Form;
 import com.cbums.model.Member;
-import com.cbums.service.FormService;
 import com.cbums.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,24 +13,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/**")
 @RequiredArgsConstructor
-public class AdminController {
+public class AdminMemberController {
 
     private final MemberService memberService;
-    private final FormService formService;
-
-    @GetMapping("/")
-    public String adminPage() {
-
-        return "/admin/admin-menu";
-    }
 
     @GetMapping("/member")
     public List<Member> memberViewPage() {
         return memberService.getMembers();
     }
 
-    @GetMapping("/form")
-    public List<Form> formViewPage() {
-        return formService.getForms();
-    }
 }
