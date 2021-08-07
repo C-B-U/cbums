be39c0e7f7a127acc8b548463f899a1362694857
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-08-07 13:11:56 +0900

    FormContentService findFormContentListByFormId 테스트

--- src/test/java/com/cbums/service/FormContentServiceTest.java
+++ src/test/java/com/cbums/service/FormContentServiceTest.java
@@ -1,6 +1,7 @@
 package com.cbums.service;
 
 import com.cbums.model.Form;
+import com.cbums.model.FormContent;
 import com.cbums.model.FormQuestion;
 import com.cbums.model.Member;
 import com.cbums.repository.FormContentRepository;
@@ -107,5 +108,76 @@ class FormContentServiceTest {
                 formContentRepository.getById(지원서내용ID.get(1)).getFormQuestion().getContent());
         assertEquals(질문3.getOpeningDatetime(),
                 formContentRepository.getById(지원서내용ID.get(2)).getFormQuestion().getOpeningDatetime());
+    }
+
+    @Test
+    public void FORM_하나_선택해서_문항보기() {
+        //given
+        HttpSession httpSession = request.getSession();
+        Member FORM_작성자 = new Member();
+        FORM_작성자.setName("홍길동");
+        FORM_작성자.setDepartment("IT경영학과, 컴퓨터공학과 복수전공");
+        FORM_작성자.setEmail("phjppo0918@kpu.ac.kr");
+        FORM_작성자.setClassNumber(2018314014);
+        FORM_작성자.setNickName("루핑투핑");
+
+        Long memberId = memberService.joinForWriteForm(FORM_작성자).getMemberId();
+        httpSession.setAttribute("login-user", memberId);
+
+        Form 지원서 = new Form();
+        지원서.setIntroduce("내애요용");
+        지원서.setTitle("제에모오오옥");
+        지원서.setRegisterNumber(1);
+        지원서.setOpenDateTime(LocalDateTime.now());
+        지원서.setCloseDateTime(LocalDateTime.now());
+        Long 지원서ID = formService.createForm(지원서);
+        httpSession.removeAttribute("login-user");
+
+        Member QUESTION_작성자 = new Member();
+        QUESTION_작성자.setName("질문자");
+        QUESTION_작성자.setDepartment("컴퓨터공학과");
+        QUESTION_작성자.setEmail("phhjkhkjhkjhj8@kpu.ac.kr");
+        QUESTION_작성자.setClassNumber(2021114014);
+        QUESTION_작성자.setNickName("질문질문");
+        memberId = memberService.joinForWriteForm(QUESTION_작성자).getMemberId();
+        httpSession.setAttribute("login-user", memberId);
+
+        FormQuestion 질문1 = new FormQuestion();
+        질문1.setContent("내용1");
+        질문1.setOpeningDatetime(LocalDateTime.now());
+        Long 질문ID1 = formQuestionService.createFormQuestion(질문1);
+
+        FormQuestion 질문2 = new FormQuestion();
+        질문2.setContent("내용2");
+        질문2.setOpeningDatetime(LocalDateTime.now());
+        Long 질문ID2 = formQuestionService.createFormQuestion(질문2);
+
+        FormQuestion 질문3 = new FormQuestion();
+        질문3.setContent("내용3");
+        질문3.setOpeningDatetime(LocalDateTime.now());
+        Long 질문ID3 = formQuestionService.createFormQuestion(질문3);
+
+        List<Long> 질문리스트 = new ArrayList<>();
+        질문리스트.add(질문ID1);
+        질문리스트.add(질문ID2);
+        질문리스트.add(질문ID3);
+
+        List<Long> 지원서내용ID = formContentService
+                .createFormContent(지원서ID, 질문리스트);
+
+        //when
+        List<FormContent> formContents = formContentService.findFormContentListByFormId(지원서ID);
+
+        //then
+        assertEquals(지원서ID,
+                formContents.get(0).getForm().getFormId());
+
+        assertEquals(질문ID1,
+                formContents.get(0).getFormQuestion().getFormQuestionId());
+        assertEquals(질문ID2,
+                formContents.get(1).getFormQuestion().getFormQuestionId());
+        assertEquals(질문ID3,
+                formContents.get(2).getFormQuestion().getFormQuestionId());
+
     }
 }
