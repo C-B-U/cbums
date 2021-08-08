ad59b24f8c1f9edac155c4382844fa367c56d1b4
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-08-08 11:08:58 +0900

    form_test 일시정지: gson -> jeckson변경 후 진행

--- src/test/java/com/cbums/controller/FormControllerTest.java
+++ src/test/java/com/cbums/controller/FormControllerTest.java
@@ -1,5 +1,6 @@
 package com.cbums.controller;
 
+import com.cbums.service.FormService;
 import lombok.RequiredArgsConstructor;
 import org.junit.Before;
 import org.junit.jupiter.api.DisplayName;
@@ -7,7 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -15,6 +18,9 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 import javax.transaction.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,17 +30,28 @@ class FormControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private WebApplicationContext ctx;
+    @Autowired
+    FormService formService;
 
     @Before
     public void setup() {
         mockMvc = MockMvcBuilders.webAppContextSetup(ctx)
                 .addFilters(new CharacterEncodingFilter("UTF-8", true))  // 한글 깨짐 처리
+                .alwaysExpect(status().isOk())
+                .alwaysExpect(content()
+                        .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .alwaysDo(print())
                 .build();
+
     }
 
     @Test
     @DisplayName("지원서 리스트_GET")
-    public void getFormList() {
+    public void getFormList() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/form"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("/member/sign-up"));
 
     }
 
--- src/test/java/com/cbums/service/InputSampleDataBase.java
+++ src/test/java/com/cbums/service/InputSampleDataBase.java
@@ -141,8 +141,9 @@ public class InputSampleDataBase {
     //지원서 답변 테스트 생성 후 제작 TODO
     public void 지원서_답변_생성() {
         HttpSession httpSession = request.getSession();
-        for(int i=25;i<175;i++) {
+        for(int i=25;i<50;i++) {
             httpSession.setAttribute("form-writer-id", (long)i);
+
         }
     }
 
