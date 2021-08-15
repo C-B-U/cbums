4af9f67931f74dd23fc9676d7e154e212e78097f
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-08-15 22:23:11 +0900

    formController 수정 완료

--- src/main/java/com/cbums/controller/FormController.java
+++ src/main/java/com/cbums/controller/FormController.java
@@ -4,17 +4,16 @@ import com.cbums.model.Form;
 import com.cbums.model.FormContent;
 import com.cbums.service.FormAnswerService;
 import com.cbums.service.FormContentService;
-import com.cbums.service.FormQuestionService;
 import com.cbums.service.FormService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 
 
 @RestController
@@ -27,40 +26,35 @@ public class FormController {
     private final FormAnswerService formAnswerService;
 
     @GetMapping("")
-    public List<Form> getFormList() throws NoSuchElementException {
-        List<Form> formList = formService.findFormList();
-        return formList;
+    public ResponseEntity<List<Form>> getFormList() {
+        return ResponseEntity.ok(formService.findFormList());
     }
-
     //기존 회원 & 신규 회원 나누어야 TODO
-    @GetMapping("/{seq}")
-    public Form getForm(HttpServletResponse response,
-                        @PathVariable("seq") Long seq) throws NullPointerException {
+    @GetMapping("/{formId}")
+    public ResponseEntity<Form> getForm(HttpServletResponse response,
+                        @PathVariable("formId") Long formId) throws NullPointerException {
 
-        Form form = formService.findFormById(seq);
+        Form form = formService.findFormById(formId);
 
         Cookie cookie = new Cookie("form-id", form.getFormId().toString());
         cookie.setMaxAge(3000);
         cookie.setPath("/");
         response.addCookie(cookie);
-        return form;
+
+        return ResponseEntity.ok(form);
     }
 
-    @GetMapping("/content/{formSeq}")
-    public List<FormContent> getFormContentQuestionList(@PathVariable("formSeq") Long formSeq) {
+    @GetMapping("/content/{formId}")
+    public ResponseEntity<List<FormContent>> getFormContentQuestionList(@PathVariable("formId") Long formId) {
 
-        List<FormContent> formContentList =
-                formContentService.findFormContentListByFormId(formSeq);
-
-        return formContentList;
+        return  ResponseEntity.ok(formContentService.findFormContentListByFormId(formId));
     }
 
     //작성자 정보
     @PostMapping(value = "/content/answer", produces = "application/json; charset=utf8")
-    public void postFormAnswer(HttpServletResponse response, @RequestBody Map<Long, String> answer) throws IOException {
+    public ResponseEntity<Void> postFormAnswer(@RequestBody Map<Long, String> answer) {
         formAnswerService.createFormAnswer(answer);
-
-        response.sendRedirect("/form/submitted");
+        return ResponseEntity.created(URI.create("/form/submitted")).build();
     }
 
 
