212ca47b3b3a92ef771a99c80bfa7eda84912b64
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-08-22 15:20:22 +0900

    api.memberController 작성중

--- src/main/java/com/cbums/core/member/dto/MemberResponse.java
+++ src/main/java/com/cbums/core/member/dto/MemberResponse.java
@@ -1,19 +1,18 @@
 package com.cbums.core.member.dto;
 
 import com.cbums.core.member.domain.Member;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class MemberResponse {
 
+    private Long memberId;
     private String email;
     private String name;
     private String nickName;
@@ -23,7 +22,9 @@ public class MemberResponse {
     private String introduce;
 
     public static MemberResponse of(Member member) {
-        return new MemberResponse(member.getEmail(),
+        return new MemberResponse(
+                member.getMemberId(),
+                member.getEmail(),
                 member.getName(),
                 member.getNickName(),
                 member.getRegisterNumber(),
