fc46b0ae62aa3598fa5bea9628c276caca20c5de
rabbit-22 <chlwlal20@gmail.com>
2021-09-07 19:29:31 +0900

    layout 수정

--- frontend/src/component/layout/FooterDefault.jsx
+++
@@ -1,9 +0,0 @@
-import React, { PureComponent } from "react";
-
-class FooterDefault extends PureComponent {
-  render() {
-    return <React.Fragment></React.Fragment>;
-  }
-}
-
-export default FooterDefault;
--- frontend/src/component/layout/FooterMobile.jsx
+++
@@ -1,9 +0,0 @@
-import React, { PureComponent } from "react";
-
-class FooterMobile extends PureComponent {
-  render() {
-    return <React.Fragment></React.Fragment>;
-  }
-}
-
-export default FooterMobile;
