4fa2dc2f12a5ec6c64612bc9da695b21d0bc53fc
rabbit-22 <chlwlal20@gmail.com>
2021-09-09 19:40:09 +0900

    수정

--- frontend/src/component/login/BoxList.jsx
+++ frontend/src/component/login/BoxList.jsx
@@ -15,10 +15,7 @@ class BoxList extends PureComponent {
           <div class="boxList__schoolName boxList__schoolName1">
             한국산업기술대학교
           </div>
-          <div class="boxList__schoolName">
-            프로그래밍 동아리
-            <span class="boxList__scoholName--emp">씨부엉</span>
-          </div>
+          <div class="boxList__schoolName">프로그래밍 동아리 씨부엉</div>
         </div>
       </React.Fragment>
     );
