eb12076e2820e7bacd9cc537f8a39b56f0169cd5
rabbit-22 <chlwlal20@gmail.com>
2021-09-13 23:28:20 +0900

    추가정보입력 페이지 swiper 추가(수정 필요)

--- frontend/src/component/signUpInput/InputContent.jsx
+++ frontend/src/component/signUpInput/InputContent.jsx
@@ -1,51 +1,74 @@
 import React, { PureComponent } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation } from "swiper";
+import "swiper/components/navigation/navigation.scss";
+import "../../css/slide.css";
+SwiperCore.use([Navigation]);
 
-const TOTAL_SLIDE = 3;
 class InputContent extends PureComponent {
   render() {
     return (
       <React.Fragment>
         <div className="article__boxList">
-          <form action="#" method="POST">
-            <ul className="boxList__infor">
-              <li>
-                <label>이름</label>
-                <input type="text" placeholder="이름 입력" name="name" />
-              </li>
-              <li>
-                <label>학과</label>
-                <input
-                  type="number"
-                  placeholder="학번 입력"
-                  name="schoolNumber"
-                />
-              </li>
-              <li>
-                <label>전화번호</label>
-                <input
-                  type="number"
-                  placeholder="전화번호 입력"
-                  name="phoneNumber"
-                />
-              </li>
-              <li>
-                <input
-                  type="submit"
-                  style={{ cursor: "pointer" }}
-                  className="boxList__infor--submit"
-                />
-              </li>
-            </ul>
-          </form>
-          <p className="boxList__controls">
-            <span className="prev">
-              <FaArrowLeft />
-            </span>
-            <span className="next">
-              <FaArrowRight />
-            </span>
-          </p>
+          <Swiper
+            className="input-container"
+            slidesPerView={1}
+            slidesPerGroup={1}
+            mousewheel={false}
+            touchRatio={false}
+            navigation={{
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            }}
+          >
+            <form action="#" method="POST">
+              <ul className="boxList__infor">
+                <li>
+                  <SwiperSlide className="box" key="input_1">
+                    <label>이름</label>
+                    <input
+                      type="text"
+                      required
+                      placeholder="이름 입력"
+                      name="name"
+                    />
+                  </SwiperSlide>
+                </li>
+                <SwiperSlide className="box" key="input_2">
+                  <label>학과</label>
+                  <input
+                    type="text"
+                    placeholder="학번 입력"
+                    name="schoolNumber"
+                    required
+                  />
+                </SwiperSlide>
+                <SwiperSlide className="box" key="input_3">
+                  <label>전화번호</label>
+                  <input
+                    type="text"
+                    placeholder="전화번호 입력"
+                    name="phoneNumber"
+                    required
+                  />
+                </SwiperSlide>
+                <SwiperSlide className="box" key="input_4">
+                  <input
+                    type="submit"
+                    style={{ cursor: "pointer" }}
+                    className="boxList__infor--submit"
+                  />
+                </SwiperSlide>
+              </ul>
+            </form>
+          </Swiper>
+          <div className="swiper-button-prev button">
+            <FaArrowLeft />
+          </div>
+          <div className="swiper-button-next button">
+            <FaArrowRight />
+          </div>
         </div>
       </React.Fragment>
     );
