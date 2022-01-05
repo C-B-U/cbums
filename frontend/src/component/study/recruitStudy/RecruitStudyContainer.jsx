037d202774548e0ad3bc91e6185ce92abb8a234e
rabbit-22 <chlwlal20@gmail.com>
2022-01-05 15:58:47 +0900

    feat: api 호출 메소드 추가

--- frontend/src/component/study/recruitStudy/RecruitStudyContainer.jsx
+++ frontend/src/component/study/recruitStudy/RecruitStudyContainer.jsx
@@ -1,16 +1,47 @@
 import React, { PureComponent } from "react";
 import RecruitStudyPost from "./RecruitStudyPost";
-
+import axios from "axios";
 class RecruitStudyContainer extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      studyName: "",
+      userName: "",
+      recruit: true,
+      maxMember: "",
+      postDate: "",
+    };
+    this.getStudy = this.getStudy.bind(this);
+  }
+  getStudy = () => {
+    axios
+      .get("/api/project")
+      .then(res => {
+        this.setState({
+          studyName: res.name,
+          userName: res.producer,
+          recruit: res.producerHidden,
+          maxMember: res.maximumMember,
+          postDate: res.registerDatetime,
+        });
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+  componentDidMount() {
+    this.getStudy();
+  }
     render() {
         return (
           <React.Fragment>
            <RecruitStudyPost
-             studyTitle="프로젝트 하실분 구해요"
+             studyName="프로젝트 하실분 구해요"
               userName="이부엉"
               postDate="2021.11.16" />
             <RecruitStudyPost
-              studyTitle="자바 스터디 모집합니다"
+              studyName="자바 스터디 모집합니다"
               userName="김부엉"
               postDate="2021.11.15"
             />
